65fc6a94ab6737dd765f410bbbe760cc2ef867d3
Joshua Biong <joshuabiong7@gmail.com>
2025-10-28 12:45:10 +0800

    Initial Commit

---
+++ Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by Joshua P. Biong on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
